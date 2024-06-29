f03ee9cf402349905d1b5f06b04d707225a9e748
baohng <bao.hng221b@gmail.com>
2024-06-30 05:55:24 +0700

    feat: Add dictionary search functionality to Dictionary component

--- final-project/src/components/user/Dictionary/Dictionary.jsx
+++ final-project/src/components/user/Dictionary/Dictionary.jsx
@@ -1,8 +1,69 @@
+import { Input, message, Card } from "antd";
+import { useState } from "react";
+
+const { Search } = Input;
+
 const Dictionary = () => {
+  const [searchResult, setSearchResult] = useState(null);
+
+  const handleSearch = async (wordNeedtoSearch) => {
+    try {
+      const response = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${wordNeedtoSearch}`
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setSearchResult(data); // Assuming you want to store the result in state
+      console.log(data); // Or handle the data as needed
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      message.error("Failed to fetch dictionary data.");
+    }
+  };
+
   return (
-    <div>
-      <h1>Dictionary</h1>
-    </div>
+    <>
+      <div>
+        <h1 className="text-left font-bold text-lg">Dictionary</h1>
+      </div>
+
+      <Search
+        className="mr-4 mt-0.5"
+        placeholder="Search word"
+        onSearch={handleSearch}
+        enterButton
+        style={{
+          width: 400,
+        }}
+      />
+      {searchResult && (
+        <Card title={searchResult[0].word}>
+          <p>
+            License:{" "}
+            <a href={searchResult[0].license.url}>
+              {searchResult[0].license.name}
+            </a>
+          </p>
+          <p>Meanings: {searchResult[0].meanings.length}</p>
+          <p>
+            Phonetics:{" "}
+            {searchResult[0].phonetics
+              .map((phonetic) => phonetic.text)
+              .join(", ")}
+          </p>
+          <p>
+            Source URLs:{" "}
+            {searchResult[0].sourceUrls.map((url) => (
+              <a href={url} key={url}>
+                {url}
+              </a>
+            ))}
+          </p>
+        </Card>
+      )}
+    </>
   );
 };
 
